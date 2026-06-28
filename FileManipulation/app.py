03708954b6a61dd32b1fc898c507ec35ab29959e
Patrick <pcallaway0603@outlook.com>
2026-06-28 12:45:11 -0400

    using .items() method and f strings to clean up text

--- FileManipulation/app.py
+++ FileManipulation/app.py
@@ -5,14 +5,22 @@ from pathlib import Path
 # Testing Pycharm and Python
 # Run on command line - python app.py
 # Add an empty list to store the lines
-lines = []
+file_extensions = {}
+
 
 folder = Path(r"D:\personal\downloads") # "." means the current directory
 for file in folder.glob("**/*"):
     if file.is_file():
-        print(file.name, "is a file")
-    else:
-        print(file.name, "is a folder")
+        ext = file.suffix # Gets the file extension
+        file_extensions[ext] = file_extensions.get(ext, 0) + 1 # Gets the number of files with the same extension the .get() method returns the value if the key exists, otherwise it returns None
+
+
+
+
+# using the .items() method to get the key and value
+# using an f string to mix variables into a string
+for ext, count in file_extensions.items():
+    print(f"{ext}: {count}")
 
 
 
