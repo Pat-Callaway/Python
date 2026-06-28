fe3222c65c110712c2ad5cfc8b5bf63843fb55e8
Patrick <pcallaway0603@gmail.com>
2026-06-28 10:17:37 -0400

    Added more lines to notes.txt) confirmed app.py works as intended

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ from builtins import print
 # Testing Pycharm and Python
 # Run on command line - python app.py
 
-print("It's running")
+with open("notes.txt") as f:
+    contents = f.read()
+
+print(contents)
