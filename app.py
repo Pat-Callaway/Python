775b1ab508c93156763f6aed68be369d643ad95d
Patrick <pcallaway0603@gmail.com>
2026-06-28 10:31:01 -0400

    lines are now held in list

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from builtins import print
 
 # Testing Pycharm and Python
 # Run on command line - python app.py
+# Add an empty list to store the lines
+lines = []
 
 with open("notes.txt") as f:
-    contents = f.read()
+    for line in f:
+        clean = line.strip().lower()
+        # The line below adds the line to the list, now you can actually work with the data.
+        lines.append(clean)
+print(lines)
 
-print(contents)
