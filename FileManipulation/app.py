a12ae534d4794b7182cfcf698a32652c401d6980
Patrick <pcallaway0603@gmail.com>
2026-06-28 11:17:26 -0400

    imported pathlib. can now search file(s) by extention and type

--- FileManipulation/app.py
+++ FileManipulation/app.py
@@ -1,10 +1,15 @@
 # Working with files
 from builtins import print
+from pathlib import Path
 
 # Testing Pycharm and Python
 # Run on command line - python app.py
 # Add an empty list to store the lines
 lines = []
+
+folder = Path(".") # "." means the current directory
+for file in folder.glob("*"):
+    print(file)
 
 with open("notes.txt") as f:
     for line in f:
@@ -12,4 +17,6 @@ with open("notes.txt") as f:
         # The line below adds the line to the list, now you can actually work with the data.
         lines.append(clean)
 print(lines)
+
+
 
