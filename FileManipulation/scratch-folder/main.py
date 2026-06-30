43fd0ab26f9d6ffa3d12103f51bd58de76c72355
Patrick <pcallaway0603@outlook.com>
2026-06-30 09:10:00 -0400

    logic now in show_menu(), created main()

--- FileManipulation/scratch-folder/main.py
+++ FileManipulation/scratch-folder/main.py
@@ -18,68 +18,77 @@ formatter = logging.Formatter('%(asctime)s = %(name)s-%(levelname)s-%(message)s'
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 
+def show_menu(): # User-defined function to
+    while True:
+        logger.info("Awaiting user input")
+        choice = input(">> ")
+        if choice == "1":
+            logger.info(f"User chose {choice}")
+            print(os.listdir())
+            logger.info("Successfully printed directories/folders...")
+        elif choice == "2":
+            logger.info(f"User chose {choice}")
+            logger.info("Attempting to list all files in current directory")
+            only_files = [f for f in os.listdir(os.getcwd()) if isfile(join(os.getcwd(), f))]
+            print(only_files)
+            logger.info("Successfully printed all files in current directory.")
+        elif choice == "3":
+            logger.info(f"User chose {choice}")
+            print("Please type which folder/file you would like to open")
+            new_directory = input(">> ")
+            logger.info(f"Attempting to sanitize {new_directory} ")
+            clean_new_directory = Path(new_directory).expanduser().resolve()  # Always make sure to sanitize user input
+            if clean_new_directory.is_dir():
+                os.chdir(clean_new_directory)
+                logger.info("Successfully changed directories")
+                print(f"You are now in {clean_new_directory}")
+            elif clean_new_directory.is_file():
+                with clean_new_directory.open("r") as file:
+                    content = file.read()
+                    print(content)
+                logger.info(f"Opening {clean_new_directory}...")
+        elif choice == "q":
+            logger.info("User quit program, closing application")
+            sys.exit()
+        else:
+            print("Please enter a valid option")
+            logger.error(f"User chose invalid option of {choice}")
+            logger.info("Prompting user again...")
+
 # Folder path for testing
-print("Which directory would you like to work in?")
+def main ():
+    print("Which directory would you like to work in?")
 
-logger.info("Obtaining user input")
+    logger.info("Obtaining user input")
 
+    while True: # while True is ALWAYS True
+        user_input = input(">> ")
+        clean_path = Path(user_input).expanduser().resolve()
+        logger.info("sanitizing user input for directory")
+        logger.info(f"checking if {clean_path} is valid..")
 
-
-
-
-while True: # while True is ALWAYS True
-    user_input = input(">>")
-    clean_path = Path(user_input).expanduser().resolve()
-    logger.info("sanitizing user input for directory")
-    logger.info(f"checking if {clean_path} is valid..")
-
-    if clean_path.is_dir():
-        logger.info("Valid directory found, changing directory")
-        os.chdir(clean_path)
-        print("You are now in " + os.getcwd())
-        break
-    else:
-        logger.warning("Invalid path")
-        print("Please enter a valid path...")
-
-print()
-print("What would you like to do?")
-print("1. List directories?\n2. List files?\n3. Navigate to a specific folder/file?\n")
-print("Please chose 1, 2, or 3...")
-
-while True:
-    logger.info("Awaiting user input")
-    user_input = input(">>")
-    if user_input == "1":
-        logger.info(f"User chose {user_input}")
-        print(os.listdir())
-        logger.info("Successfully printed directories/folders...")
-        break
-    elif user_input == "2":
-        logger.info(f"User chose {user_input}")
-        logger.info("Attempting to list all files in current directory")
-        only_files = [f for f in os.listdir(os.getcwd()) if isfile(join(os.getcwd(), f))]
-        print(only_files)
-        logger.info("Successfully printed all files in current directory.")
-        break
-    elif user_input == "3":
-        logger.info(f"User chose {user_input}")
-        print("Please type which folder/file you would like to open")
-        new_directory = input(">> ")
-        logger.info(f"Attempting to sanitize {new_directory} ")
-        clean_new_directory = Path(new_directory).expanduser().resolve() # Always make sure to sanitize user input
-        if clean_new_directory.is_dir():
-            os.chdir(clean_new_directory)
-            logger.info("Successfully changed directories")
-            print(f"You are now in {clean_new_directory}")
-            break
-        elif clean_new_directory.is_file():
-            open(clean_new_directory) # os.O_RDWR is the read and write FLAG for os.open()
-            logger.info(f"Opening {clean_new_directory}...")
+        if clean_path.is_dir():
+            logger.info("Valid directory found, changing directory")
+            os.chdir(clean_path)
+            print("You are now in " + os.getcwd())
             break
         else:
-            logger.error(f"User chose invalid option of {user_input}")
-            logger.info("Prompting user again...")
+            logger.warning("Invalid path")
+            print("Please enter a valid path...")
+
+    print()
+    print("What would you like to do?")
+    print("1. List directories?\n2. List files?\n3. Navigate to a specific folder/file?\n4. Press 'q' to quit...")
+    print("Please chose 1, 2, or 3...")
+
+    show_menu()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
 
 
 
